86a8beaec7884400f147c1853257f5a06a5615f0
Dmitry Molokovich <molokovich.dima@gmail.com>
2021-01-04 23:30:05 +0300

    bash script to generate commits

---
+++ generate_user_commits.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+if [[ $# < 4 ]]; then
+	echo "Not enough arguments";
+	echo "usage: sh generate_users_commits.sh <name> <born_year> <school> <email> without angle parenthesis";
+	exit 1;
+fi
+
+NAME=$1
+BORN_YEAR=$2
+SCHOOL=$3
+EMAIL=$4
+
+$CURRENT_BRANCH="feature/user$NAME"
+cd git_lab2_lesson2 
+
+function commit_changes() {
+	git add .
+	git commit -m "created git_lab2_lesson2/$1" --author "$NAME <$EMAIL>"
+	git push origin $CURRENT_BRANCH
+}
+
+git checkout -b $CURRENT_BRANCH
+echo $BORN_YEAR > git_lab2_lesson2/$NAME.txt
+commit_changes $NAME.txt
+
+$CURRENT_BRANCH="feature/list$NAME"
+git checkout -b $CURRENT_BRANCH
+echo $NAME > list.txt;
+commit_changes list.txt
+
+$CURRENT_BRANCH="feature/school$NAME"
+git checkout -b $CURRENT_BRANCH
+echo $SCHOOL > ${NAME}School.txt
+commit_changes ${NAME}School.txt
+
+echo "Done";
+exit 0;
