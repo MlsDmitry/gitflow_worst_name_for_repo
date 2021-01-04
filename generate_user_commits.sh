a56f0a349bbc003319c6f21e4cac6e295242fdd2
Dmitry Molokovich <molokovich.dima@gmail.com>
2021-01-04 23:38:58 +0300

    removing everything

--- generate_user_commits.sh
+++ generate_user_commits.sh
@@ -12,7 +12,7 @@ SCHOOL=$3
 EMAIL=$4
 
 CURRENT_BRANCH="feature/user$NAME"
-cd git_lab2_lesson2 
+cd git_lab2_lesson2/
 
 function commit_changes() {
 	git add .
@@ -21,18 +21,21 @@ function commit_changes() {
 }
 
 git checkout -b $CURRENT_BRANCH
-echo $BORN_YEAR > git_lab2_lesson2/$NAME.txt
+echo $BORN_YEAR > $NAME.txt
 commit_changes $NAME.txt
+cd ..
 
 $CURRENT_BRANCH="feature/list$NAME"
 git checkout -b $CURRENT_BRANCH
 echo $NAME > list.txt;
 commit_changes list.txt
+cd ..
 
 $CURRENT_BRANCH="feature/school$NAME"
 git checkout -b $CURRENT_BRANCH
 echo $SCHOOL > ${NAME}School.txt
 commit_changes ${NAME}School.txt
+cd ..
 
 echo "Done";
 exit 0;
